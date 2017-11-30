f29e7c8f18cfe48deda61c47879c43642501f07f
GreywindGit <GreywindGit@users.noreply.github.com>
2017-11-30 09:23:59 +0100

    added basic winform structure with all the ui elements

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace SearchDojo
 {
-    partial class Form1
+    partial class Matcher
     {
         /// <summary>
         /// Required designer variable.
@@ -28,12 +28,102 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.rtbText = new System.Windows.Forms.RichTextBox();
+            this.rtbMatches = new System.Windows.Forms.RichTextBox();
+            this.buttonSearch = new System.Windows.Forms.Button();
+            this.labelText = new System.Windows.Forms.Label();
+            this.labelMatches = new System.Windows.Forms.Label();
+            this.labelSearchPattern = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(73, 380);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(242, 22);
+            this.textBox1.TabIndex = 0;
+            // 
+            // rtbText
+            // 
+            this.rtbText.Location = new System.Drawing.Point(21, 28);
+            this.rtbText.Name = "rtbText";
+            this.rtbText.Size = new System.Drawing.Size(294, 320);
+            this.rtbText.TabIndex = 1;
+            this.rtbText.Text = "";
+            // 
+            // rtbMatches
+            // 
+            this.rtbMatches.Location = new System.Drawing.Point(350, 28);
+            this.rtbMatches.Name = "rtbMatches";
+            this.rtbMatches.Size = new System.Drawing.Size(285, 320);
+            this.rtbMatches.TabIndex = 2;
+            this.rtbMatches.Text = "";
+            // 
+            // buttonSearch
+            // 
+            this.buttonSearch.Location = new System.Drawing.Point(350, 379);
+            this.buttonSearch.Name = "buttonSearch";
+            this.buttonSearch.Size = new System.Drawing.Size(75, 23);
+            this.buttonSearch.TabIndex = 3;
+            this.buttonSearch.Text = "Search";
+            this.buttonSearch.UseVisualStyleBackColor = true;
+            // 
+            // labelText
+            // 
+            this.labelText.AutoSize = true;
+            this.labelText.Location = new System.Drawing.Point(21, 5);
+            this.labelText.Name = "labelText";
+            this.labelText.Size = new System.Drawing.Size(35, 17);
+            this.labelText.TabIndex = 4;
+            this.labelText.Text = "Text";
+            // 
+            // labelMatches
+            // 
+            this.labelMatches.AutoSize = true;
+            this.labelMatches.Location = new System.Drawing.Point(350, 5);
+            this.labelMatches.Name = "labelMatches";
+            this.labelMatches.Size = new System.Drawing.Size(61, 17);
+            this.labelMatches.TabIndex = 5;
+            this.labelMatches.Text = "Matches";
+            // 
+            // labelSearchPattern
+            // 
+            this.labelSearchPattern.AutoSize = true;
+            this.labelSearchPattern.Location = new System.Drawing.Point(12, 383);
+            this.labelSearchPattern.Name = "labelSearchPattern";
+            this.labelSearchPattern.Size = new System.Drawing.Size(54, 17);
+            this.labelSearchPattern.TabIndex = 6;
+            this.labelSearchPattern.Text = "Pattern";
+            // 
+            // Matcher
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(660, 449);
+            this.Controls.Add(this.labelSearchPattern);
+            this.Controls.Add(this.labelMatches);
+            this.Controls.Add(this.labelText);
+            this.Controls.Add(this.buttonSearch);
+            this.Controls.Add(this.rtbMatches);
+            this.Controls.Add(this.rtbText);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Matcher";
+            this.Text = "Matcher";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.RichTextBox rtbText;
+        private System.Windows.Forms.RichTextBox rtbMatches;
+        private System.Windows.Forms.Button buttonSearch;
+        private System.Windows.Forms.Label labelText;
+        private System.Windows.Forms.Label labelMatches;
+        private System.Windows.Forms.Label labelSearchPattern;
     }
 }
 
